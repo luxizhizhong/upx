3f9543f14894fe11baa1d9cb67d279dfd975cee4
d1y <chenhonzhou@gmail.com>
2019-10-31 12:43:32 +0800

    add message

---
+++ client/dev/src/config/msg.js
@@ -0,0 +1,13 @@
+export default {
+  info: {
+    httpCheck: {
+      path: '请输入正确格式的路径',
+      port: '输入的端口将会自动检测是否占用',
+      note: '输入备注, 记忆一下这个环境'
+    }
+  },
+  error: {
+    wrongPath: '错误的路径😂',
+    wrongFormatPath: '路径格式错误😂'
+  }
+}
